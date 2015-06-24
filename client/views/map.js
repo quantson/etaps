4d01c733572adc7a1141c670b12e57a314fcae5f
Quentin Maurice <q.maurice@hotmail.fr>
2015-06-25 00:14:33 +0100

    initialized map with dburles package

---
+++ client/views/map.js
@@ -0,0 +1,16 @@
+Template.map.helpers({
+	MapOptions: function () {
+		if (GoogleMaps.loaded()) {
+			return {
+				center: {lat: 0, lng: 0},
+				zoom: 4,
+				disableDefaultUI: true,
+				zoomControl: true,
+         		zoomControlOptions: {
+		          style: google.maps.ZoomControlStyle.SMALL,
+		          position: google.maps.ControlPosition.RIGHT_TOP
+		        },
+			};
+		}
+	}
+});
